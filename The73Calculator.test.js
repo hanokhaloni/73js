b97303f1379f0f07f8ff86f6b5fa6201da941816
hanokh <hanokh.aloni@gmail.com>
2022-01-31 18:45:08 +0200

    Added 2 more tests, and the duplication is painful

--- The73Calculator.test.js
+++ The73Calculator.test.js
@@ -1,11 +1,20 @@
-const The73Calculator = require('./The73Calculator');
+const The73Calculator = require("./The73Calculator");
 
 describe("Validator", () => {
-    //const the73Calculator = new The73Calculator();
-  
-    test('useif', () => {
-        expect(The73Calculator.useif(3)).toBe(7);
-        expect(The73Calculator.useif(7)).toBe(3);
+  //const the73Calculator = new The73Calculator();
 
-      });
+  test("useif", () => {
+    expect(The73Calculator.useif(3)).toBe(7);
+    expect(The73Calculator.useif(7)).toBe(3);
+  });
+
+  test("useXor", () => {
+    expect(The73Calculator.useXor(3)).toBe(7);
+    expect(The73Calculator.useXor(7)).toBe(3);
+  });
+  test("useArray", () => {
+    expect(The73Calculator.useArray(3)).toBe(7);
+    expect(The73Calculator.useArray(7)).toBe(3);
+  });
+
 });
