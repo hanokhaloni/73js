957d06b334c21f92b31ed6c33e7b5e1de8fcafe3
hanokh <hanokh.aloni@gmail.com>
2022-01-31 18:20:58 +0200

    Added default test

---
+++ The73Calculator.test.js
@@ -0,0 +1,12 @@
+const The73Calculator = require('./The73Calculator');
+
+describe("Validator", () => {
+    //const the73Calculator = new The73Calculator();
+  
+    test('hello', () => {
+        expect(The73Calculator.hello()).toBe("Hello!!");
+      });
+      
+});
+
+;
---
+++ the73Calculator.js
@@ -0,0 +1,9 @@
+module.exports = class The73Calculator {
+    constructor() {
+    }
+
+    static hello() {
+        return "Hello!!";
+    }
+
+}
